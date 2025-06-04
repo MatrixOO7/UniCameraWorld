561e0dccaf71673b55451e27def3e880f9b6ea3b
natrix-1987 <tom.krejcovsky@seznam.cz>
2025-06-04 16:05:58 +0200

    Add UniThemeManager

---
+++ UniCameraWorld/UniThemeManager/UniThemeManager.cpp
@@ -0,0 +1,5 @@
+#include "UniThemeManager.h"
+
+UniThemeManager::UniThemeManager ( QObject *parent ) {
+
+}
---
+++ UniCameraWorld/UniThemeManager/UniThemeManager.h
@@ -0,0 +1,22 @@
+#ifndef UNITHEMEMANAGER_V_1
+#define UNITHEMEMANAGER_V_1
+
+#include <QObject>
+#include <QQmlEngine>
+
+class UniThemeManager : public QObject {
+    Q_OBJECT
+
+public:
+    UniThemeManager ( QObject *parent = nullptr );
+
+signals:
+
+private slots:
+
+
+
+private:
+};
+
+#endif
